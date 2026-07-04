58f14f5c92547b7f2a055c2b40b69510b33828f4
Gabriel <oliveiracgabriel052@gmail.com>
2026-07-04 13:30:34 -0300

    feat(restricoes): adicionar busca, filtros e badges coloridos para os grupos

--- app/Enums/CorTroca.php
+++ app/Enums/CorTroca.php
@@ -42,4 +42,15 @@ enum CorTroca: string
             self::LARANJA => 'border-l-orange-400 dark:border-l-orange-400',
         };
     }
+
+    public function badgeClass(): string
+    {
+        return match ($this) {
+            self::VERMELHA => 'bg-red-100 text-red-800 dark:bg-red-950/40 dark:text-red-300 border border-red-200 dark:border-red-800',
+            self::AZUL => 'bg-blue-100 text-blue-800 dark:bg-blue-950/40 dark:text-blue-300 border border-blue-200 dark:border-blue-800',
+            self::VERDE => 'bg-green-100 text-green-800 dark:bg-green-950/40 dark:text-green-300 border border-green-200 dark:border-green-800',
+            self::AMARELA => 'bg-yellow-100 text-yellow-800 dark:bg-yellow-950/30 dark:text-yellow-300 border border-yellow-200 dark:border-yellow-800',
+            self::LARANJA => 'bg-orange-100 text-orange-800 dark:bg-orange-950/40 dark:text-orange-300 border border-orange-200 dark:border-orange-800',
+        };
+    }
 }
